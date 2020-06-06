30fb47853e85affb3460a61f0203a02304522e0f
diego_neri <diego@diegoneri.com>
2020-06-05 21:30:49 -0300

    Finaliza o exercício

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace IdadeAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int dezoitoAnos = 18;
+            DateTime dataNascimento = new DateTime(1986, 11, 05);
+            DateTime dataDezoitoAnos = dataNascimento.AddYears(dezoitoAnos);
+
+            string dataNascimentoLonga = dataNascimento.ToLongDateString();
+            string dataDezoitoAnosLonga = dataDezoitoAnos.ToLongDateString();
+
+            Console.WriteLine($"Eu nasci em {dataNascimentoLonga}");
+            Console.WriteLine($"Aniversário de {dezoitoAnos} anos em {dataDezoitoAnosLonga}");
+        }
+    }
+}
